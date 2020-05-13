09a015693d2b724e178af449336881440fa6bc72
Dameon Smith <dameonsmith76@gmail.com>
2020-05-13 03:52:36 +0100

    Made it so status bar functions as buttons

--- blocks.h
+++ blocks.h
@@ -2,29 +2,30 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	
-    {"", "~/.local/bin/statusbar/battery",						5,		0},
-
-    {"", "~/.local/bin/statusbar/clock",						5,		0},
-
-    {"", "~/.local/bin/statusbar/cpu", 10, 0},
+    //{"", "~/.local/bin/statusbar/cpu", 10, 3},
     
-    {"", "~/.local/bin/statusbar/crypto", 0, 0},
 
-    {"", "~/.local/bin/statusbar/help-icon", 0, 0},
+    /*{"", "~/.local/bin/statusbar/memory", 1, 11},*/
 
-    {"", "~/.local/bin/statusbar/internet", 5, 0},
+    /*{"", "~/.local/bin/statusbar/pacpackages", 1, 5},*/
 
-    /*{"", "~/.local/bin/statusbar/memory", 1, 0},*/
+    //{"", "~/.local/bin/statusbar/crypto", 0, 4},
+    
+    //{"", "~/.local/bin/statusbar/help-icon", 0, 13},
+    
+    //{"", "~/.local/bin/statusbar/popupgrade",   1,      9},
 
-    /*{"", "~/.local/bin/statusbar/pacpackages", 1, 0},*/
+    {"", "~/.local/bin/statusbar/torrent",      20,      5},
 
-    /*{"", "~/.local/bin/statusbar/popupgrade", 1, 0},*/
+    {"", "~/.local/bin/statusbar/weather",      18000,   4},
 
-    {"", "~/.local/bin/statusbar/torrent", 1, 0},
+    {"", "~/.local/bin/statusbar/internet",     5, 12},
 
-    {"", "~/.local/bin/statusbar/volume", 1, 0},
+    {"", "~/.local/bin/statusbar/volume",       0,      3},
 
-    {"", "~/.local/bin/statusbar/weather", 1, 0}
+    {"", "~/.local/bin/statusbar/battery",      5,      2},
+    
+    {"", "~/.local/bin/statusbar/clock",	    60,	    1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
