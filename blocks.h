62ba65b83448696e86cfa17453342a0dc8597958
Dameon Smith <dameonsmith76@gmail.com>
2020-05-22 14:45:47 -0600

    Updated blocks to use scripts from bin to status.

--- blocks.h
+++ blocks.h
@@ -1,31 +1,31 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	
-    //{"", "~/.local/bin/statusbar/cpu", 10, 3},
-    
 
-    /*{"", "~/.local/bin/statusbar/memory", 1, 11},*/
+    //{"", "~/.local/scripts/statusbar/cpu", 10, 3},
 
-    /*{"", "~/.local/bin/statusbar/pacpackages", 1, 5},*/
 
-    //{"", "~/.local/bin/statusbar/crypto", 0, 4},
-    
-    //{"", "~/.local/bin/statusbar/help-icon", 0, 13},
-    
-    //{"", "~/.local/bin/statusbar/popupgrade",   1,      9},
+    /*{"", "~/.local/scripts/statusbar/memory", 1, 11},*/
 
-    {"", "~/.local/bin/statusbar/torrent",      20,      5},
+    /*{"", "~/.local/scripts/statusbar/pacpackages", 1, 5},*/
 
-    {"", "~/.local/bin/statusbar/weather",      18000,   4},
+    //{"", "~/.local/scripts/statusbar/crypto", 0, 4},
 
-    {"", "~/.local/bin/statusbar/internet",     5, 12},
+    //{"", "~/.local/scripts/statusbar/help-icon", 0, 13},
 
-    {"", "~/.local/bin/statusbar/volume",       0,      3},
+    //{"", "~/.local/scripts/statusbar/popupgrade",   1,      9},
 
-    {"", "~/.local/bin/statusbar/battery",      5,      2},
-    
-    {"", "~/.local/bin/statusbar/clock",	    60,	    1},
+    {"", "~/.local/scripts/statusbar/torrent",      20,      5},
+
+    {"", "~/.local/scripts/statusbar/weather",      18000,   4},
+
+    {"", "~/.local/scripts/statusbar/internet",     5, 12},
+
+    {"", "~/.local/scripts/statusbar/volume",       0,      3},
+
+    {"", "~/.local/scripts/statusbar/battery",      5,      2},
+
+    {"", "~/.local/scripts/statusbar/clock",	    60,	    1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
